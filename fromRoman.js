0ffe71f94235b88c0a84ca78becae19a06c08496
Laci Szűcs <ifrozen@gmail.com>
2015-03-08 15:55:22 +0100

    little shorter

--- fromRoman.js
+++ fromRoman.js
@@ -4,10 +4,6 @@ var negateIfHasBiggerSuccessors = function(v, idx, whole) {
   return ((_.max(whole.slice(idx)) > v) ? -1 : 1) * v;
 };
 
-var summarize = function(sum, num) {
-  return sum + num;
-};
-
 module.exports = function fromRomanFactory(romanDigits) {
   var digitValues = _(romanDigits).invert().mapValues(_.ary(parseInt, 1)).value();
   var romanCharAbsoluteValue = function(char) {
@@ -17,6 +13,6 @@ module.exports = function fromRomanFactory(romanDigits) {
     return _(r.split('')).
         map(romanCharAbsoluteValue).
         map(negateIfHasBiggerSuccessors).
-        reduce(summarize);
+        sum();
   };
 };
